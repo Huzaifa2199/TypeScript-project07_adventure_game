4f0e0355cc7017dabbae48ecab1ae20832db2cac
Huzaifa <huzaifa.m2199@gmail.com>
2023-02-05 03:43:22 +0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+
+interface ansType {
+    atHome: string,
+    atMountains: string,
+    atForest: string
+
+}
+
+const answers: ansType = await inquirer.prompt([
+    {
+        type: "list",
+        name: "atHome",
+        choices: ["Mountains", "Forest"],
+        message: "You are at your home. Select place where you want to go",
+        // when(answers) {
+        //     return answers.atHome == "Home"
+        // },
+    },
+    {
+        type: "list",
+        name: "atMountains",
+        choices: ["Forest", "Home"],
+        message: "You are at mountains. Select place where you want to go",
+        when(answers) {
+            return answers.atHome == "Mountains"
+        },
+    },
+    {
+        type: "list",
+        name: "atForest",
+        choices: ["Home", "Mountains"],
+        message: "You are at forest. Select place where you want to go",
+        when(answers) {
+            return answers.atHome == "Forest"
+        },
+    },
+])
+
+
+
+if (answers.atHome) {
+    console.log("Thankyou for playing");
+}
